c692c84e1f493c2809c6dd97b4a2976b4d94d02b
Guilherme Oliveira <gwoliveira@gmail.com>
2018-02-27 20:00:05 -0300

    repositorio cliente

---
+++ src/main/java/br/una/projetodesoftware/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package br.una.projetodesoftware.repository;
+
+import br.una.projetodesoftware.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
