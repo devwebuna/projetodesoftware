d128c205b2bdaf41b3a41ea987425f9e9877d800
Guilherme Oliveira <gwoliveira@gmail.com>
2018-02-27 19:39:26 -0300

    setup projeto

---
+++ src/main/java/br/una/projetodesoftware/ProjetodesoftwareApplication.java
@@ -0,0 +1,12 @@
+package br.una.projetodesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodesoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodesoftwareApplication.class, args);
+	}
+}
